8d6feab9a011f383ff6236319f04dde0ccc0e5d9
= <7185@users.noreply.github.com>
2018-08-03 19:38:54 +0200

    fix(bot): fix move thread issues

--- pwlib/bobinot.py
+++ pwlib/bobinot.py
@@ -15,8 +15,10 @@ class Bot(PW):
         self.connect('7185.fr', 6670)
         self.following = 0
         self.move_speed = 10
+        self.current_move_thread = 0
 
     def move(self, dest_x, dest_z):
+        thread_id = self.current_move_thread
         tick = 200
         length = ((dest_x - dest_z) ** 2 + (self.x - self.z) ** 2) ** 0.5
         direction = atan2(dest_x - self.x, dest_z - self.z) + pi
@@ -28,6 +30,8 @@ class Bot(PW):
         else:
             gaps = [[dest_x, dest_z]]
         for p in gaps:
+            if thread_id != self.current_move_thread:
+                break
             self.set_position(p[0], self.y, p[1], yaw=direction)
             self.send_position()
             sleep(tick / 1e3)
@@ -38,62 +42,64 @@ class Bot(PW):
         self.set_position(1430, 10, 960)
         self.send_position()
 
-    def on_self_message_public(self,msg):
-        self.display('>'+self.nickname+'< '+msg)
+    def on_self_message_public(self, msg):
+        self.display('>' + self.nickname + '< ' + msg)
 
-    def on_message_public(self,user,msg):
-        self.display('<'+user+'> '+msg)
+    def on_message_public(self, user, msg):
+        self.display('<' + user + '> ' + msg)
         m = msg.split()
-        if 'alo' in m: 
-            self.message_public(user+': slt')
-        elif '!loc' in m:  
-            self.message_public(user+': je suis en '+str(int(self.x))+', '+str(int(self.y))+', '+str(int(self.z)))
+        if 'alo' in m:
+            self.message_public(user + ': slt')
+        elif '!loc' in m:
+            self.message_public(
+                user + ': je suis en ' + str(int(self.x)) + ', ' + str(int(self.y)) + ', ' + str(int(self.z)))
         elif '!list' in m:
             s = ''
             for u in self.userlist:
-                s+=str(u)+':'+self.userlist[u].nickname+' '
+                s += str(u) + ':' + self.userlist[u].nickname + ' '
             self.message_public(s)
         elif '!follow' in m:
-            u = self.getidbynick(user)
+            u = self.get_id_by_nick(user)
             if self.following != u:
                 self.following = u
             else:
                 self.following = 0
         elif '!come' in m:
-            uid = self.getidbynick(user)
+            uid = self.get_id_by_nick(user)
             u = self.userlist[uid]
+            self.current_move_thread += 1
             thread = threading.Thread(target=self.move, args=(u.x, u.z))
             thread.daemon = True
             thread.start()
         elif '!speed' in m and len(m) > 1 and m[1].isdigit():
             self.move_speed = int(m[1])
 
-    def on_message_private(self,user,msg):
-        self.display('-'+user+'- '+msg)
-        if 'alo' in msg.split(): 
-            self.message_private(user,'slt')
+    def on_message_private(self, user, msg):
+        self.display('-' + user + '- ' + msg)
+        if 'alo' in msg.split():
+            self.message_private(user, 'slt')
 
-    def on_avatar_position(self,user,x,y,z,pi,ya):
+    def on_avatar_position(self, user, x, y, z, pi, ya):
         if self.following == user:
             u = self.userlist[user]
-            self.set_position(u.x,u.y,u.z,u.pitch,u.yaw)
+            self.set_position(u.x, u.y, u.z, u.pitch, u.yaw)
             self.send_position()
 
-    def on_user_join(self,user):
-        self.display('* '+user+' has joined')
+    def on_user_join(self, user):
+        self.display('* ' + user + ' has joined')
 
-    def on_user_part(self,user):
-        self.display('* '+user+' has left')
+    def on_user_part(self, user):
+        self.display('* ' + user + ' has left')
 
-    def on_connect_error(self,reason):
+    def on_connect_error(self, reason):
         if reason == SC_ER_NICKINUSE:
             self.display('! Diconnected (nick already taken)')
         elif reason == SC_ER_ERRONEOUSNICK:
             self.display('! Diconnected (illegal nick)')
 
-    def display(self,msg):
-        print(strftime("[%H:%M:%S] ")+msg)
+    def display(self, msg):
+        print(strftime("[%H:%M:%S] ") + msg)
+
 
 b = Bot()
 b.run()
-
--- pwlib/pwlib.py
+++ pwlib/pwlib.py
@@ -40,18 +40,21 @@ from protocol import *
 
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), errors='backslashreplace', line_buffering=True)
 
+
 def update_protocol(filename='../server/Protocol.h'):
-    '''python -c "import pwlib; pwlib.update_protocol()" > protocol.py'''
+    """python -c "import pwlib; pwlib.update_protocol()" > protocol.py"""
     f = open(filename, 'r')
     for line in f.readlines():
         l = list(filter(None, line.split(' ')))
-        if l[0] == '#define' and len(l)==3:
-            print(l[1]+'='+l[2][:-1])
+        if l[0] == '#define' and len(l) == 3:
+            print(l[1] + '=' + l[2][:-1])
+
 
 def qstring(string):
-    msg = bytes(string,'utf-16-be')
-    l = struct.pack('<I',struct.unpack('>I',bytes(c_uint32(len(msg))))[0])
-    return l+msg
+    msg = bytes(string, 'utf-16-be')
+    l = struct.pack('<I', struct.unpack('>I', bytes(c_uint32(len(msg))))[0])
+    return l + msg
+
 
 class User(object):
     def __init__(self, nick=""):
@@ -62,31 +65,29 @@ class User(object):
         self.pitch = 0
         self.yaw = 0
 
-    def set_position(self,x=0.0,y=0.0,z=0.0,pitch=0.0,yaw=0.0):
+    def set_position(self, x=0.0, y=0.0, z=0.0, pitch=0.0, yaw=0.0):
         self.x = x
         self.y = y
         self.z = z
         self.pitch = pitch
         self.yaw = yaw
 
+
 class PW(User):
-    def __init__(self, logging_enabled = True):
-        super(PW,self).__init__()
+    def __init__(self, logging_enabled=True):
+        super(PW, self).__init__()
         self.logging_enabled = logging_enabled
         self.connected = False
         self.enabled = True
         self.handlers = {}
         self.userlist = {}
+        self.sk = None
 
-    def connect(self, host, port = 6667, use_ssl=False):
-        '''Etablish a connection to a server'''
+    def connect(self, host, port=6667, use_ssl=False):
+        """Etablish a connection to a server"""
         self.log('@ Connecting to %s port %d' % (host, port))
 
         self.sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        if use_ssl:
-            import ssl
-            self.sk = ssl.wrap_socket(self.sk)
 
         self.sk.connect((host, port))
         self.log('@ Connected')
@@ -109,7 +110,7 @@ class PW(User):
         thread.start()
         return thread
 
-    def getidbynick(self,nick):
+    def get_id_by_nick(self, nick):
         for i in self.userlist.items():
             if i[1].nickname == nick:
                 return i[0]
@@ -122,27 +123,26 @@ class PW(User):
         self.sk.send(b)
         self.log('> ' + str(binascii.hexlify(b)))
 
-    def send(self,code,msg):
-        c = struct.pack('<H',struct.unpack('>H',bytes(c_uint16(code)))[0])
+    def send(self, code, msg):
+        c = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(code)))[0])
         s = qstring(msg)
-        l = struct.pack('<H',struct.unpack('>H',bytes(c_uint16(len(c+s))))[0])
-        self.raw(l+c+s)
+        l = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(len(c + s))))[0])
+        self.raw(l + c + s)
 
     def ask_list(self):
-        c = struct.pack('<H',struct.unpack('>H',bytes(c_uint16(CS_USER_LIST)))[0])
-        l = struct.pack('<H',struct.unpack('>H',bytes(c_uint16(len(c))))[0])
-        self.raw(l+c)
+        c = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(CS_USER_LIST)))[0])
+        l = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(len(c))))[0])
+        self.raw(l + c)
 
     def send_position(self):
-        c = struct.pack('<H',struct.unpack('>H',bytes(c_uint16(CS_AVATAR_POSITION)))[0])
-        x = struct.pack('<d',struct.unpack('>d',bytes(c_double(self.x)))[0])
-        y = struct.pack('<d',struct.unpack('>d',bytes(c_double(self.y)))[0])
-        z = struct.pack('<d',struct.unpack('>d',bytes(c_double(self.z)))[0])
-        pi = struct.pack('<d',struct.unpack('>d',bytes(c_double(self.pitch)))[0])
-        ya = struct.pack('<d',struct.unpack('>d',bytes(c_double(self.yaw)))[0])
-        l = struct.pack('<H',struct.unpack('>H',bytes(c_uint16(len(c+x+y+z+pi+ya))))[0])
-        self.raw(l+c+x+y+z+pi+ya)
-        
+        c = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(CS_AVATAR_POSITION)))[0])
+        x = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.x)))[0])
+        y = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.y)))[0])
+        z = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.z)))[0])
+        pi = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.pitch)))[0])
+        ya = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.yaw)))[0])
+        l = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(len(c + x + y + z + pi + ya))))[0])
+        self.raw(l + c + x + y + z + pi + ya)
 
     def auth(self):
         self.send(CS_AUTH, self.nickname)
@@ -152,7 +152,7 @@ class PW(User):
         self._callback('on_self_message_public', msg)
 
     def message_private(self, user, msg=''):
-        self.send(CS_MSG_PRIVATE, str(user)+':'+msg)
+        self.send(CS_MSG_PRIVATE, str(user) + ':' + msg)
         self._callback('on_self_message_public', user, msg)
 
     def _callback(self, name, *parameters):
@@ -164,57 +164,55 @@ class PW(User):
             f(*parameters)
 
     def _process_packet(self, b):
-        self.log('< '+ str(b))
-        plength = struct.unpack('>H',b[0:2])[0]
-        code = struct.unpack('>H',b[2:4])[0]
-        if (code == SC_ER_NICKINUSE):
-            self._callback('on_connect_error',SC_ER_NICKINUSE)
-        elif (code == SC_ER_ERRONEOUSNICK):
-            self._callback('on_connect_error',SC_ER_ERRONEOUSNICK)
-        elif (code == SC_MSG_PUBLIC):
-            slength = struct.unpack('>L',b[4:8])[0]
+        self.log('< ' + str(b))
+        plength = struct.unpack('>H', b[0:2])[0]
+        code = struct.unpack('>H', b[2:4])[0]
+        if code == SC_ER_NICKINUSE:
+            self._callback('on_connect_error', SC_ER_NICKINUSE)
+        elif code == SC_ER_ERRONEOUSNICK:
+            self._callback('on_connect_error', SC_ER_ERRONEOUSNICK)
+        elif code == SC_MSG_PUBLIC:
+            slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
-            if len(s)*2 == slength:
+            if len(s) * 2 == slength:
                 params = s.split(':')
                 self._callback('on_message_public', params[0], ':'.join(params[1:]))
-        elif (code == SC_MSG_PRIVATE):
-            slength = struct.unpack('>L',b[4:8])[0]
+        elif code == SC_MSG_PRIVATE:
+            slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
-            if len(s)*2 == slength:
+            if len(s) * 2 == slength:
                 params = s.split(':')
                 self._callback('on_message_private', params[0], ':'.join(params[1:]))
-        elif (code == SC_USER_LIST): 
-            slength = struct.unpack('>L',b[4:8])[0]
+        elif code == SC_USER_LIST:
+            slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
-            if len(s)*2 == slength:
+            if len(s) * 2 == slength:
                 l = s.split(';')
                 self.userlist.clear()
                 for c in l:
-                    self.userlist[int(c.split(':')[0])]=User(c.split(':')[1])
+                    self.userlist[int(c.split(':')[0])] = User(c.split(':')[1])
                 self._callback('on_user_list')
-        elif (code == SC_AVATAR_POSITION):
-            u = struct.unpack('>H',b[4:6])[0]
-            x = struct.unpack('>d',b[6:14])[0]
-            y = struct.unpack('>d',b[14:22])[0]
-            z = struct.unpack('>d',b[22:30])[0]
-            pitch = struct.unpack('>d',b[30:38])[0]
-            yaw = struct.unpack('>d',b[38:46])[0]
+        elif code == SC_AVATAR_POSITION:
+            u = struct.unpack('>H', b[4:6])[0]
+            x = struct.unpack('>d', b[6:14])[0]
+            y = struct.unpack('>d', b[14:22])[0]
+            z = struct.unpack('>d', b[22:30])[0]
+            pitch = struct.unpack('>d', b[30:38])[0]
+            yaw = struct.unpack('>d', b[38:46])[0]
             if u in self.userlist.keys():
-                self.userlist[u].set_position(x,y,z,pitch,yaw)
-                self._callback('on_avatar_position',u,x,y,z,pitch,yaw)
-        elif (code == SC_USER_JOIN):
-            slength = struct.unpack('>L',b[4:8])[0]
+                self.userlist[u].set_position(x, y, z, pitch, yaw)
+                self._callback('on_avatar_position', u, x, y, z, pitch, yaw)
+        elif code == SC_USER_JOIN:
+            slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
-            if len(s)*2 == slength:
+            if len(s) * 2 == slength:
                 self.ask_list()
                 self._callback('on_user_join', s)
-        elif (code == SC_USER_PART):
-            slength = struct.unpack('>L',b[4:8])[0]
+        elif code == SC_USER_PART:
+            slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
-            if len(s)*2 == slength:
+            if len(s) * 2 == slength:
                 self.ask_list()
                 self._callback('on_user_part', s)
         else:
             self.log("! Can't handle packet.")
-            
- 
