8ab351c211e6b1fc2ab4e545fa70a668f8f4ca45
= <7185@users.noreply.github.com>
2019-06-11 23:47:45 +0200

    refactor(bot): use asyncio with typing hints and fix a couple of bugs
    closes: #9

--- pwlib/bobinot.py
+++ pwlib/bobinot.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-import threading
-from pwlib import *
+import asyncio
+from pwlib import PW, SC_ER_NICKINUSE, SC_ER_ERRONEOUSNICK
 from math import atan2, pi
-from time import sleep, strftime
+from time import strftime
 
 
 class Bot(PW):
@@ -12,12 +12,11 @@ class Bot(PW):
         super(Bot, self).__init__(*args, **kwargs)
         self.logging_enabled = False
         self.nickname = 'bobinot'
-        self.connect('7185.fr', 6670)
         self.following = 0
         self.move_speed = 5
         self.current_move_thread = 0
 
-    def move(self, dest_x, dest_z):
+    async def move(self, dest_x: float, dest_z: float) -> None:
         thread_id = self.current_move_thread
         tick = 200
         length = ((dest_x - self.x) ** 2 + (dest_z - self.z) ** 2) ** 0.5
@@ -33,31 +32,31 @@ class Bot(PW):
             if thread_id != self.current_move_thread:
                 break
             self.set_position(p[0], self.y, p[1], yaw=direction)
-            self.send_position()
-            sleep(tick / 1e3)
+            await self.send_position()
+            await asyncio.sleep(tick / 1e3)
 
-    def on_connected(self):
-        self.auth()
-        self.message_public('alo')
+    async def on_connected(self) -> None:
+        await self.auth()
+        await self.message_public('alo')
         self.set_position(1430, 10, 960)
-        self.send_position()
+        await self.send_position()
 
-    def on_self_message_public(self, msg):
+    async def on_self_message_public(self, msg: str) -> None:
         self.display('>' + self.nickname + '< ' + msg)
 
-    def on_message_public(self, user, msg):
+    async def on_message_public(self, user: str, msg: str) -> None:
         self.display('<' + user + '> ' + msg)
         m = msg.split()
         if 'alo' in m:
-            self.message_public(user + ': slt')
+            await self.message_public(user + ': slt')
         elif '!loc' in m:
-            self.message_public(
+            await self.message_public(
                 user + ': je suis en ' + str(int(self.x)) + ', ' + str(int(self.y)) + ', ' + str(int(self.z)))
         elif '!list' in m:
             s = ''
             for u in self.userlist:
                 s += str(u) + ':' + self.userlist[u].nickname + ' '
-            self.message_public(s)
+            await self.message_public(s)
         elif '!follow' in m:
             u = self.get_id_by_nick(user)
             if self.following != u:
@@ -68,38 +67,36 @@ class Bot(PW):
             uid = self.get_id_by_nick(user)
             u = self.userlist[uid]
             self.current_move_thread += 1
-            thread = threading.Thread(target=self.move, args=(u.x, u.z))
-            thread.daemon = True
-            thread.start()
+            asyncio.ensure_future(self.move(u.x, u.z))
         elif '!speed' in m and len(m) > 1 and m[1].isdigit():
             self.move_speed = int(m[1])
 
-    def on_message_private(self, user, msg):
+    async def on_message_private(self, user: str, msg: str) -> None:
         self.display('-' + user + '- ' + msg)
         if 'alo' in msg.split():
-            self.message_private(user, 'slt')
+            await self.message_private(user, 'slt')
 
-    def on_avatar_position(self, user, x, y, z, pi, ya):
+    async def on_avatar_position(self, user: str, x: float, y: float, z: float, pi: float, ya: float) -> None:
         if self.following == user:
             u = self.userlist[user]
             self.set_position(u.x, u.y, u.z, u.pitch, u.yaw)
-            self.send_position()
+            await self.send_position()
 
-    def on_user_join(self, user):
+    async def on_user_join(self, user: str) -> None:
         self.display('* ' + user + ' has joined')
 
-    def on_user_part(self, user):
+    async def on_user_part(self, user: str) -> None:
         self.display('* ' + user + ' has left')
 
-    def on_connect_error(self, reason):
+    async def on_connect_error(self, reason: str) -> None:
         if reason == SC_ER_NICKINUSE:
             self.display('! Diconnected (nick already taken)')
         elif reason == SC_ER_ERRONEOUSNICK:
             self.display('! Diconnected (illegal nick)')
 
-    def display(self, msg):
+    def display(self, msg: str) -> None:
         print(strftime("[%H:%M:%S] ") + msg)
 
 
-b = Bot()
+b = Bot('7185.fr', 6670)
 b.run()
--- pwlib/pwlib.py
+++ pwlib/pwlib.py
@@ -24,26 +24,25 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-__version__ = 'pwlib, youpi!'
-
-ENCODING = 'utf-8'
-
-import socket
-import threading
-import sys
-import io
-from ctypes import c_uint16, c_uint32, c_double
-import struct
+import asyncio
 import binascii
+import io
+import struct
+import sys
+from typing import Callable
+from ctypes import c_uint16, c_uint32, c_double
+from contextlib import suppress
 from protocol import *
+
+__version__ = '0.2'
+
 
 HEARTBEAT_RATE = 60
 
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), errors='backslashreplace', line_buffering=True)
 
 
-def update_protocol(filename='../server/Protocol.h'):
+def update_protocol(filename: str='../server/Protocol.h') -> None:
     """python -c "import pwlib; pwlib.update_protocol()" > protocol.py"""
     f = open(filename, 'r')
     for line in f.readlines():
@@ -52,14 +51,31 @@ def update_protocol(filename='../server/Protocol.h'):
             print(l[1] + '=' + l[2][:-1])
 
 
-def qstring(string):
+def qstring(string: str) -> bytes:
     msg = bytes(string, 'utf-16-be')
     l = struct.pack('<I', struct.unpack('>I', bytes(c_uint32(len(msg))))[0])
     return l + msg
 
 
+class Timer:
+    def __init__(self, timeout: int, callback: Callable) -> None:
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.ensure_future(self._job())
+
+    async def _job(self) -> None:
+        await asyncio.sleep(self._timeout)
+        await self._callback()
+
+    async def cancel(self) -> None:
+        if not self._task.cancelled():
+            self._task.cancel()
+            with suppress(asyncio.CancelledError):
+                await self._task
+
+
 class User:
-    def __init__(self, nick=""):
+    def __init__(self, nick: str="") -> None:
         self.nickname = nick
         self.x = 0
         self.y = 0
@@ -67,7 +83,7 @@ class User:
         self.pitch = 0
         self.yaw = 0
 
-    def set_position(self, x=0.0, y=0.0, z=0.0, pitch=0.0, yaw=0.0):
+    def set_position(self, x: float = 0.0, y: float = 0.0, z: float = 0.0, pitch: float = 0.0, yaw: float = 0.0):
         self.x = x
         self.y = y
         self.z = z
@@ -76,73 +92,83 @@ class User:
 
 
 class PW(User):
-    def __init__(self, logging_enabled=True):
+    def __init__(self, host: str, port: int, logging_enabled: bool=True) -> None:
         super(PW, self).__init__()
+        self.host = host
+        self.port = port
         self.logging_enabled = logging_enabled
         self.connected = False
         self.enabled = True
         self.handlers = {}
         self.userlist = {}
-        self.sk = None
-        self.heartbeat = threading.Timer(HEARTBEAT_RATE, self.send_heartbeat)
+        self.reader = None
+        self.writer = None
+        self.heartbeat = None
 
-    def connect(self, host, port=6667, use_ssl=False):
-        """Etablish a connection to a server"""
-        self.log('@ Connecting to %s port %d' % (host, port))
+    def log(self, txt: str) -> None:
+        if self.logging_enabled:
+            print(txt)
 
-        self.sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        self.sk.connect((host, port))
-        self.log('@ Connected')
-        self.connected = True
-        self.heartbeat.start()
-        self._callback('on_connected')
-
-    def run(self):
-        while self.enabled:
-            recv = self.sk.recv(0x100)
-            self._process_packet(recv)
-
-        self.connected = False
-        self.log('@ Disconnected')
-        self._callback('on_disconnnected')
-        self.enabled = False
-
-    def run_threaded(self):
-        thread = threading.Thread(target=self.run)
-        thread.setDaemon(True)
-        thread.start()
-        return thread
-
-    def get_id_by_nick(self, nick):
+    def get_id_by_nick(self, nick: str) -> int:
         for i in self.userlist.items():
             if i[1].nickname == nick:
                 return i[0]
 
-    def log(self, txt):
-        if self.logging_enabled:
-            print(txt)
+    async def send(self, msg: bytes) -> None:
+        self.writer.write(msg)
+        self.writer.drain()
 
-    def raw(self, b):
-        self.sk.send(b)
-        if self.heartbeat.is_alive():
-            self.heartbeat.cancel()
-        self.heartbeat = threading.Timer(HEARTBEAT_RATE, self.send_heartbeat)
-        self.heartbeat.start()
+    async def recv(self) -> asyncio.StreamReader:
+        data = await self.reader.read(0x100)
+        return data
+
+    async def raw(self, b: bytes):
+        await self.send(b)
+        if self.heartbeat:
+            await self.heartbeat.cancel()
+        self.heartbeat = Timer(HEARTBEAT_RATE, self.send_heartbeat)
         self.log('> ' + str(binascii.hexlify(b)))
 
-    def send(self, code, msg):
+    async def connect(self, host: str, port: int) -> None:
+        self.reader, self.writer = await asyncio.open_connection(host=host, port=port)
+
+        self.log('@ Connected')
+        self.connected = True
+        await self.send_heartbeat()
+        await self._callback('on_connected')
+
+    async def loop(self) -> None:
+        await self.connect(self.host, self.port)
+        while self.enabled:
+            r = await self.recv()
+            await self._process_packet(r)
+
+        self.connected = False
+        self.log('@ Disconnected')
+        await self._callback('on_disconnected')
+        self.enabled = False
+
+    def run(self) -> None:
+        # all the following will be replaced by: asyncio.run(self.loop())
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self.loop())
+        finally:
+            loop.run_until_complete(loop.shutdown_asyncgens())
+            loop.close()
+
+    async def send_msg(self, code: int, msg: str) -> None:
         c = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(code)))[0])
         s = qstring(msg)
         l = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(len(c + s))))[0])
-        self.raw(l + c + s)
+        await self.raw(l + c + s)
 
-    def ask_list(self):
+    async def ask_list(self) -> None:
         c = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(CS_USER_LIST)))[0])
         l = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(len(c))))[0])
-        self.raw(l + c)
+        await self.raw(l + c)
 
-    def send_position(self):
+    async def send_position(self) -> None:
         c = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(CS_AVATAR_POSITION)))[0])
         x = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.x)))[0])
         y = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.y)))[0])
@@ -150,52 +176,53 @@ class PW(User):
         pi = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.pitch)))[0])
         ya = struct.pack('<d', struct.unpack('>d', bytes(c_double(self.yaw)))[0])
         l = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(len(c + x + y + z + pi + ya))))[0])
-        self.raw(l + c + x + y + z + pi + ya)
+        await self.raw(l + c + x + y + z + pi + ya)
 
-    def send_heartbeat(self):
+    async def send_heartbeat(self) -> None:
         c = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(CS_HEARTBEAT)))[0])
         l = struct.pack('<H', struct.unpack('>H', bytes(c_uint16(len(c))))[0])
-        self.raw(l + c)
+        await self.raw(l + c)
 
-    def auth(self):
-        self.send(CS_AUTH, self.nickname)
+    async def auth(self) -> None:
+        await self.send_msg(CS_AUTH, self.nickname)
 
-    def message_public(self, msg=''):
-        self.send(CS_MSG_PUBLIC, msg)
-        self._callback('on_self_message_public', msg)
+    async def message_public(self, msg: str='') -> None:
+        await self.send_msg(CS_MSG_PUBLIC, msg)
+        await self._callback('on_self_message_public', msg)
 
-    def message_private(self, user, msg=''):
-        self.send(CS_MSG_PRIVATE, str(user) + ':' + msg)
-        self._callback('on_self_message_public', user, msg)
+    async def message_private(self, user: str, msg: str='') -> None:
+        await self.send_msg(CS_MSG_PRIVATE, str(self.get_id_by_nick(user)) + ':' + msg)
+        await self._callback('on_self_message_private', user, msg)
 
-    def _callback(self, name, *parameters):
+    async def _callback(self, name: str, *parameters) -> None:
         for inst in [self] + list(self.handlers.values()):
             f = getattr(inst, name, None)
             if not hasattr(f, '__call__'):
                 continue
             self.log('calling %s() on instance %r' % (name, inst))
-            f(*parameters)
+            if f is not None:
+                await f(*parameters)
 
-    def _process_packet(self, b):
+    async def _process_packet(self, b: bytes) -> None:
         self.log('< ' + str(b))
         plength = struct.unpack('>H', b[0:2])[0]
         code = struct.unpack('>H', b[2:4])[0]
         if code == SC_ER_NICKINUSE:
-            self._callback('on_connect_error', SC_ER_NICKINUSE)
+            await self._callback('on_connect_error', SC_ER_NICKINUSE)
         elif code == SC_ER_ERRONEOUSNICK:
-            self._callback('on_connect_error', SC_ER_ERRONEOUSNICK)
+            await self._callback('on_connect_error', SC_ER_ERRONEOUSNICK)
         elif code == SC_MSG_PUBLIC:
             slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
             if len(s) * 2 == slength:
                 params = s.split(':')
-                self._callback('on_message_public', params[0], ':'.join(params[1:]))
+                await self._callback('on_message_public', params[0], ':'.join(params[1:]))
         elif code == SC_MSG_PRIVATE:
             slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
             if len(s) * 2 == slength:
                 params = s.split(':')
-                self._callback('on_message_private', params[0], ':'.join(params[1:]))
+                await self._callback('on_message_private', params[0], ':'.join(params[1:]))
         elif code == SC_USER_LIST:
             slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
@@ -204,7 +231,7 @@ class PW(User):
                 self.userlist.clear()
                 for c in l:
                     self.userlist[int(c.split(':')[0])] = User(c.split(':')[1])
-                self._callback('on_user_list')
+                await self._callback('on_user_list')
         elif code == SC_AVATAR_POSITION:
             u = struct.unpack('>H', b[4:6])[0]
             x = struct.unpack('>d', b[6:14])[0]
@@ -214,18 +241,18 @@ class PW(User):
             yaw = struct.unpack('>d', b[38:46])[0]
             if u in self.userlist.keys():
                 self.userlist[u].set_position(x, y, z, pitch, yaw)
-                self._callback('on_avatar_position', u, x, y, z, pitch, yaw)
+                await self._callback('on_avatar_position', u, x, y, z, pitch, yaw)
         elif code == SC_USER_JOIN:
             slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
             if len(s) * 2 == slength:
-                self.ask_list()
-                self._callback('on_user_join', s)
+                await self.ask_list()
+                await self._callback('on_user_join', s)
         elif code == SC_USER_PART:
             slength = struct.unpack('>L', b[4:8])[0]
             s = b[8:].decode('utf-16-be')
             if len(s) * 2 == slength:
-                self.ask_list()
-                self._callback('on_user_part', s)
+                await self.ask_list()
+                await self._callback('on_user_part', s)
         else:
             self.log("! Can't handle packet.")
